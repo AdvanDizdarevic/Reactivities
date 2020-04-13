22967efc992d12cbd2e6f88f89d58088749e703e
Advan Dizdarevic <adi@stylelabs.com>
2020-04-13 12:04:15 -0400

    additional changes

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,12 +1,11 @@
 using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using Application.Activities;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Persistance;
+
 
 namespace API.Controllers
 {
@@ -38,6 +37,17 @@ namespace API.Controllers
         public async Task<ActionResult<Unit>> Create(Create.Command command)
         {
             return await _mediator.Send(command);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id,Edit.Command command){
+            command.Id = id;
+            return await _mediator.Send(command);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Delete(Guid id){
+             return await _mediator.Send(new Delete.Command{Id = id});
         }
     }
 }
