0fd4a2ef337cadffbe29bb8d803879140ddd823a
Advan Dizdarevic <adi@stylelabs.com>
2020-04-13 15:03:12 -0400

    React CRUD

---
+++ client-app/src/features/nav/NavBar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Menu, Container, Button } from "semantic-ui-react";
+
+export const NavBar = () => {
+  return (
+    <div>
+      <Menu fixed="top" inverted>
+        <Container>
+          <Menu.Item header>
+            <img
+              src="./assets/logo.png"
+              alt="logo"
+              style={{ marginRight: "10px" }}
+            />
+            Reactivities
+          </Menu.Item>
+          <Menu.Item name="Activities" />
+          <Menu.Item>
+            <Button positive content="Create Activity" />
+          </Menu.Item>
+        </Container>
+      </Menu>
+    </div>
+  );
+};
