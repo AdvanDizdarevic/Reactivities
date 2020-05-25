c904668015f7f58884d1c2d59dae7bbad3def42d
Advan Dizdarevic <adi@stylelabs.com>
2020-05-24 20:35:41 -0400

    Refactoring for Mobx

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,8 +1,124 @@
-import { observable } from "mobx";
-import { createContext } from "react";
+import { observable, action, computed, configure, runInAction } from "mobx";
+import { createContext, SyntheticEvent } from "react";
+import { IActivity } from "../models/activity";
+import agent from "../api/agent";
+
+configure({ enforceActions: "always" });
 
 class ActivityStore {
-  @observable title = "Hello from Mobx";
+  @observable activityRegistry = new Map();
+  @observable activities: IActivity[] = [];
+  @observable selectedActivity: IActivity | undefined;
+  @observable loadinInitial = false;
+  @observable editMode = false;
+  @observable submitting = false;
+  @observable target = "";
+
+  @computed get activitiesByDate() {
+    return Array.from(this.activityRegistry.values()).sort(
+      (a, b) => Date.parse(a.date) - Date.parse(b.date)
+    );
+  }
+
+  @action loadActivities = async () => {
+    this.loadinInitial = true;
+    try {
+      const activities = await agent.Activities.list();
+      runInAction("loading activities", () => {
+        activities.forEach((activity) => {
+          activity.date = activity.date.split(".")[0];
+          this.activityRegistry.set(activity.id, activity);
+        });
+        this.loadinInitial = false;
+      });
+    } catch (error) {
+      runInAction("loading activities", () => {
+        this.loadinInitial = false;
+      });
+      console.log(error);
+    }
+  };
+
+  @action createActivity = async (activity: IActivity) => {
+    this.submitting = true;
+    try {
+      await agent.Activities.create(activity);
+      runInAction("creating activity", () => {
+        this.activityRegistry.set(activity.id, activity);
+        this.editMode = false;
+        this.submitting = false;
+      });
+    } catch (error) {
+      console.log(error);
+      runInAction("create activity error", () => {
+        this.submitting = false;
+      });
+    }
+  };
+
+  @action editActivity = async (activity: IActivity) => {
+    this.submitting = true;
+    try {
+      await agent.Activities.update(activity);
+      runInAction("editing activity", () => {
+        this.activityRegistry.set(activity.id, activity);
+        this.selectedActivity = activity;
+        this.editMode = false;
+        this.submitting = false;
+      });
+    } catch (error) {
+      runInAction("edit activity error", () => {
+        this.submitting = false;
+      });
+
+      console.log(error);
+    }
+  };
+
+  @action openCreateForm = () => {
+    this.editMode = true;
+    this.selectedActivity = undefined;
+  };
+
+  @action openEditForm = (id: string) => {
+    this.selectedActivity = this.activityRegistry.get(id);
+    this.editMode = true;
+  };
+
+  @action cancelSelectedAcitvity = () => {
+    this.selectedActivity = undefined;
+  };
+
+  @action cancelFormOpen = () => {
+    this.editMode = false;
+  };
+
+  @action deleteActivity = async (
+    event: SyntheticEvent<HTMLButtonElement>,
+    id: string
+  ) => {
+    this.submitting = true;
+    this.target = event.currentTarget.name;
+    try {
+      await agent.Activities.delete(id);
+      runInAction("deletinig activity", () => {
+        this.activityRegistry.delete(id);
+        this.submitting = false;
+        this.target = "";
+      });
+    } catch (error) {
+      runInAction("deletinig activity error", () => {
+        this.submitting = false;
+        this.target = "";
+      });
+      console.log(error);
+    }
+  };
+
+  @action selectActivity = (id: string) => {
+    this.selectedActivity = this.activityRegistry.get(id);
+    this.editMode = false;
+  };
 }
 
 export default createContext(new ActivityStore());
--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,11 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Menu, Container, Button } from "semantic-ui-react";
+import ActivityStore from "../../app/stores/activityStore";
+import { observer } from "mobx-react-lite";
 
-interface IProps {
-  openCreateForm: () => void;
-}
-
-export const NavBar: React.FC<IProps> = ({ openCreateForm }) => {
+const NavBar: React.FC = () => {
+  const activityStore = useContext(ActivityStore);
   return (
     <div>
       <Menu fixed="top" inverted>
@@ -21,7 +20,7 @@ export const NavBar: React.FC<IProps> = ({ openCreateForm }) => {
           <Menu.Item name="Activities" />
           <Menu.Item>
             <Button
-              onClick={openCreateForm}
+              onClick={activityStore.openCreateForm}
               positive
               content="Create Activity"
             />
@@ -31,3 +30,5 @@ export const NavBar: React.FC<IProps> = ({ openCreateForm }) => {
     </div>
   );
 };
+
+export default observer(NavBar);
